453cfa5a32be616720e4d5884ad22bd80058a41c
Alucard <smpark@ezstorage.co.kr>
2020-12-27 12:35:00 +0900

    first upload

--- src/App.js
+++ src/App.js
@@ -1,24 +1,42 @@
-import logo from './logo.svg';
-import './App.css';
+import {
+  Button,
+  createMuiTheme,
+  Paper,
+  Switch,
+  ThemeProvider,
+} from "@material-ui/core";
+import { green, lime } from "@material-ui/core/colors";
+import { useState } from "react";
 
 function App() {
+  const [dark, setDark] = useState(false);
+
+  const theme = createMuiTheme({
+    palette: {
+      type: dark ? "dark" : "light",
+      primary: {
+        main: lime[500],
+      },
+      secondary: {
+        main: green["A400"],
+      },
+    },
+  });
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ThemeProvider theme={theme}>
+      <Paper style={{ width: "100%", height: "100vh" }}>
+        <Switch checked={dark} onChange={(e) => setDark(e.target.checked)} />
+        <Button>Default Button</Button>
+        <Button color="primary">Primary Button</Button>
+        <Button color="secondary" variant="contained">
+          Secondary Button
+        </Button>
+        <Button color="secondary" variant="outlined">
+          Secondary Button
+        </Button>
+      </Paper>
+    </ThemeProvider>
   );
 }
 
